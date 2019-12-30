7bf76381fcf520bcaa85a52de1f49d389721ddbd
smm850740 <saurabh.malpure@tatatechnologies.com>
2019-12-30 14:56:21 +0530

    -Applciation Class Added -SharedPreference Added

---
+++ app/src/main/java/mit/android/irctcfooddelivery/util/IRCTCFOODEXPRESSApplication.java
@@ -0,0 +1,20 @@
+package mit.android.irctcfooddelivery.util;
+
+import android.app.Application;
+import android.content.Context;
+
+public class IRCTCFOODEXPRESSApplication extends Application {
+
+    private static Context mIRCTCFOODEXPRESSApplicationContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mIRCTCFOODEXPRESSApplicationContext=getApplicationContext();
+    }
+
+    public static Context getIRCTCFOODEXPRESSApplicationContext(){
+        return mIRCTCFOODEXPRESSApplicationContext;
+    }
+
+}
